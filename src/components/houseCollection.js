d6cd12c0415f59d8894dbcff00c5f94e4bfe37fe
Rana Najam <rnajam147@icloud.com>
2024-07-16 17:05:22 +0500

    House collections

--- src/components/houseCollection.js
+++ src/components/houseCollection.js
@@ -1,10 +1,24 @@
-export function createMaterials() {
-    const hero = document.createElement("section");
-    hero.className = "unit-house-collection";
+export function createHouseCollection() {
+  const hero = document.createElement("section");
+  hero.className = "unit-house-collection";
 
-    hero.innerHTML = `
-    
+  hero.innerHTML = `
+    <div class="container">
+      <span class="title">
+        Cosy House Collection at Home
+      </span>
+      <div class="row">
+        <img src="/material-1.png" alt=""/>
+        <img src="/material-2.png" alt=""/>
+        <img src="/material-3.png" alt=""/>
+        <img src="/material-1.png" alt=""/>
+        <img src="/material-2.png" alt=""/>
+      </div>
+      <div class="button">
+        Shop now
+      </div>
+    </div>
   `;
 
-    return hero;
+  return hero;
 }
