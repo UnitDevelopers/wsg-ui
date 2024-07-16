e16af0eed0da28c1af5c3bea170fac808ca1a06d
Rana Najam <rnajam147@icloud.com>
2024-07-16 17:48:11 +0500

    footer ui

---
+++ src/components/footer.js
@@ -0,0 +1,40 @@
+export function createFooter() {
+    const hero = document.createElement("section");
+    hero.className = "unit-footer";
+
+    hero.innerHTML = `
+    <span class="main-text">
+        Get in touch with us, we’re always here for you
+    </span>
+    <div class="flex-container">
+        <div class="flex-container">
+            <div class="icon-container">
+                <img src="/footer-1.png" alt=""/>
+            </div>
+            <div class="support-container">
+                <span class="title">
+                    We're available to chat live
+                </span>
+                <span class="btn-text">
+                    Chat with us
+                </span>
+            </div>
+        </div>
+        <div class="flex-container">
+            <div class="icon-container">
+                <img src="/footer-2.png" alt=""/>
+            </div>
+            <div class="support-container">
+                <span class="title">
+                    support@cosyhousecollection.com
+                </span>
+                <span class="btn-text">
+                    Email Us
+                </span>
+            </div>
+        </div>
+    </div>
+  `;
+
+    return hero;
+}
