3fe497c7f7fac17767ae8e455afc0d6865b3e64a
Rana Najam <rnajam147@icloud.com>
2024-07-17 23:19:40 +0500

    add: hero section responsiveness

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,10 +3,10 @@ import "slick-carousel";
 
 export function createHero() {
   const hero = document.createElement("section");
-  hero.className = "unit-hero";
+  hero.className = "unit-homepage-hero";
 
   $(document).ready(function () {
-    $(".unit-hero-carousel").slick({
+    $(".unit-homepage-hero-carousel").slick({
       autoplay: true,
       autoplaySpeed: 2000,
       dots: true,
@@ -19,17 +19,17 @@ export function createHero() {
   });
 
   hero.innerHTML = `
-  <div class="unit-hero-overlay">
+  <div class="unit-homepage-hero-overlay">
   <span class="heading">Affordable Luxury, Delivered Daily</span>
   <span class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut eget egestas tellus.</span>
   <div class="button">SHOP NOW</div>
 </div>
-<div class="unit-hero-carousel">
+<div class="unit-homepage-hero-carousel">
   <div><img src="/hero-image.png" alt="Image 1" /></div>
   <div><img src="/category-duvet.png" alt="Image 2" /></div>
   <div><img src="/category-bed-sheet.png" alt="Image 3" /></div>
 </div>
-<div class="unit-hero-gradient"></div>
+<div class="unit-homepage-hero-gradient"></div>
   `;
 
   return hero;
